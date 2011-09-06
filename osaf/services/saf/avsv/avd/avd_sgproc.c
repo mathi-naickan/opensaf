2dd4b1c90d5da24d63269e9d31c8a63b01432bfd
Praveen <pmalviya@goahead.com>
2011-09-06 19:30:36 +0530

    AVSv:responding to SI admin operation when target node goes down #1945

--- osaf/services/saf/avsv/avd/avd_sgproc.c
+++ osaf/services/saf/avsv/avd/avd_sgproc.c
@@ -1730,6 +1730,7 @@ void avd_node_susi_fail_func(AVD_CL_CB *cb, AVD_AVND *avnd)
 {
 	AVD_SU *i_su;
 	AVD_COMP *i_comp;
+	AVD_SU_SI_REL *l_susi;
 
 	TRACE_ENTER();
 
@@ -1754,6 +1755,16 @@ void avd_node_susi_fail_func(AVD_CL_CB *cb, AVD_AVND *avnd)
 			i_su->pend_cbk.invocation = 0;
 			i_su->pend_cbk.admin_oper = 0;
 		}
+
+
+                /* Check if there was any admin operations going on SIs. */
+                for (l_susi = i_su->list_of_susi; l_susi != NULL; l_susi = l_susi->su_next) {
+                        if (l_susi->si->invocation != 0) {
+                                immutil_saImmOiAdminOperationResult(cb->immOiHandle, l_susi->si->invocation,
+                                                SA_AIS_ERR_TIMEOUT);
+                                l_susi->si->invocation = 0;
+                        }
+                }
 
 		i_comp = i_su->list_of_comp;
 		while (i_comp != NULL) {
@@ -1858,6 +1869,15 @@ void avd_node_susi_fail_func(AVD_CL_CB *cb, AVD_AVND *avnd)
 			i_su->pend_cbk.invocation = 0;
 			i_su->pend_cbk.admin_oper = 0;
 		}
+
+                /* Check if there was any admin operations going on SIs. */
+                for (l_susi = i_su->list_of_susi; l_susi != NULL; l_susi = l_susi->su_next) {
+                        if (l_susi->si->invocation != 0) {
+                                immutil_saImmOiAdminOperationResult(cb->immOiHandle, l_susi->si->invocation,
+                                                SA_AIS_ERR_TIMEOUT);
+                                l_susi->si->invocation = 0;
+                        }
+                }
 
 		i_comp = i_su->list_of_comp;
 		while (i_comp != NULL) {
