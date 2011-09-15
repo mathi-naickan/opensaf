2e748ee9e50c1652e402b12f3790dcb667f881c3
Anders Bjornerstedt <anders.bjornerstedt@ericsson.com>
2011-09-15 10:50:32 +0200

    immsv: Epoch set directly after loading completed (#1987)

--- osaf/services/saf/immsv/immnd/immnd_proc.c
+++ osaf/services/saf/immsv/immnd/immnd_proc.c
@@ -1598,7 +1598,6 @@ uns32 immnd_proc_server(uns32 *timeout)
 					immnd_ackToNid(rc);
 				}
 			} else if (immModel_getLoader(cb) == 0) {	/*Success in loading */
-				immnd_adjustEpoch(cb, SA_TRUE);
 				cb->mState = IMM_SERVER_READY;
 				immnd_ackToNid(NCSCC_RC_SUCCESS);
 				LOG_NO("SERVER STATE: IMM_SERVER_LOADING_SERVER --> IMM_SERVER_READY");
@@ -1647,7 +1646,6 @@ uns32 immnd_proc_server(uns32 *timeout)
 			immnd_ackToNid(rc);
 		}
 		if (immModel_getLoader(cb) == 0) {
-			immnd_adjustEpoch(cb, SA_TRUE);
 			immnd_ackToNid(NCSCC_RC_SUCCESS);
 			cb->mState = IMM_SERVER_READY;
 			cb->mJobStart = now;
