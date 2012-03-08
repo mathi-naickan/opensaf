97c0e2dd10e6c874cd9a4ccfcf431f1ac33a6969
Nagendra <nkumar@goahead.com>
2012-03-08 17:02:56 +0530

    AVSV: Marked existing pg record as valid in failover case #2142

--- osaf/services/saf/avsv/avnd/avnd_pg.c
+++ osaf/services/saf/avsv/avnd/avnd_pg.c
@@ -293,6 +293,7 @@ static uns32 avnd_process_pg_track_start_rsp_on_fover(AVND_CB *cb, AVND_PG *pg,
 			if (NULL == pg_mem) {
 				pg_mem = avnd_pgdb_mem_rec_add(cb, pg, mem_info);
 			} else {
+				pg_mem->mem_exist = TRUE;
 				/* Compare the data and give call back of there is mis-match */
 				if (pg_mem->info.member.haState != mem_info->member.haState) {
 					pg_mem->info.member.haState = mem_info->member.haState;
