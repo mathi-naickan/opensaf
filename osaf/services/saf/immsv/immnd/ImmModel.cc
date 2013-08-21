8686898f294a8052ef39c11dece2bd62640e0b25
Anders Bjornerstedt <anders.bjornerstedt@ericsson.com>
2013-08-21 09:50:33 +0200

    IMM: Ignore spurious/redundant ccb-apply from client [#535]
    A spurious and redundant ccb-apply generated by a faulty code elsewhere shall
    be ignored by the immnd server.
    Bugs in the imma library or new bugs introduced in the immnd server in the
    future should never be able to cause such a serious error as an inconsistent
    commit. The problem here was that the arrival of the second apply reaching the
    ccb already in critical at some processor(s) caused the ccb to get *aborted*
    in imm-ram while being in critical. That should never be allowed to happen.
    A ccb in critical is waiting on the commit decision from PBE and PBE alone.
    Patch also fixes a potential source of redundant apply in imma_om_api.c
    potentially caused by mutithreaded applications (incorrect usage of handle).

--- osaf/services/saf/immsv/immnd/ImmModel.cc
+++ osaf/services/saf/immsv/immnd/ImmModel.cc
@@ -4020,8 +4020,10 @@ ImmModel::ccbApply(SaUint32T ccbId,
                 ccb->mAdminOwnerId);
             ccb->mVeto = SA_AIS_ERR_BAD_HANDLE;
         } else if(ccb->mState > IMM_CCB_READY) {
-            LOG_NO("ERR_FAILED_OPERATION: Ccb not in correct state (%u) for Apply", ccb->mState);
-            ccb->mVeto = SA_AIS_ERR_FAILED_OPERATION;
+            LOG_NO("Ccb <%u> not in correct state (%u) for Apply ignoring request",
+                ccb->mId, ccb->mState);
+            err = SA_AIS_ERR_ACCESS_DENIED;
+            goto ignore;
         }
 
         osafassert(reqConn==0 || (ccb->mOriginatingConn == reqConn));
@@ -4090,6 +4092,7 @@ ImmModel::ccbApply(SaUint32T ccbId,
             }
         }
     }
+ ignore:
     
     TRACE_LEAVE();
     return err;
