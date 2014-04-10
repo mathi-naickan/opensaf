e4db8172ee9e82ce3ab358734945b7c0470cf511
Hans Feldt <hans.feldt@ericsson.com>
2014-04-10 13:32:04 +0200

    avsv: include and use comp capability in SUSI msg [#574]
    When the AMF node director receives the SUSI ASGN message, for each CSI it reads
    the saAmfCSType attribute from IMM.
    
    When the AMF node director receives the SUSI MOD message, for each CSI it reads
    the saAmfCtCompCapability attribute from the comp and csi associated
    SaAmfCtCsType instance.
    
    In any of these cases if IMM does not respond in a timely manner the amfnd
    process will be aborted and the node restarted.
    
    By including component capability (an int) in the SUSI ASG msg, these reads
    from IMM can be completely skipped in the node director.

--- osaf/services/saf/avsv/avnd/avnd_su.c
+++ osaf/services/saf/avsv/avnd/avnd_su.c
@@ -306,10 +306,20 @@ uint32_t avnd_evt_avd_info_su_si_assign_evh(AVND_CB *cb, AVND_EVT *evt)
 	cb->rcv_msg_id = info->msg_id;
 
 	if (info->msg_act == AVSV_SUSI_ACT_ASGN) {
-		/* SI rank was introduced in version 5 of the amfnd protocol, if the
-		 * message version is older then that, read SI rank from IMM */
-		if (evt->msg_fmt_ver < 5)
+		/* SI rank and CSI capability (originally from SaAmfCtCsType) was
+		 * introduced in version 5 of the amfnd protocol. If the
+		 * message version is older, take action. */
+		if (evt->msg_fmt_ver < 5) {
+			AVSV_SUSI_ASGN *csi;
+
+			/* indicate that capability is invalid for later use when
+			 * creating CSI_REC */
+			for (csi = info->list; csi != NULL; csi = csi->next) {
+				csi->capability = ~0;
+			}
+
 			info->si_rank = get_sirank(&info->si_name);
+		}
 	} else {
 		if (info->si_name.length > 0) {
 			if (avnd_su_si_rec_get(cb, &info->su_name, &info->si_name) == NULL)
