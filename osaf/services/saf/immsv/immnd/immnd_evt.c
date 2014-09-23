373fd38b467cf0a0d15a1456b7df27047ee349b5
Anders Bjornerstedt <anders.bjornerstedt@ericsson.com>
2014-09-23 10:54:53 +0200

    imm: In immnd_evt_proc_ccb_compl_rsp skip to end if PBE-OI is detached [#1096]
    The case is where a CCB has passed validation by all involved OIs. Normally the
    next step, if PBE is enabled, is to send the completed callbac to the PBE-OI.
    The PBE actually commits the ccb/sqlite-transaction in the completed callback.
    
    But in this case, the PBE has detached after the ccb has been built up, but
    before imm-ram can send the completed-callback to the PBE. In this case it is
    in princple ok for imm-ram to abort the CCB, since the PBE has not received
    any completed callback and can not have comitted this ccb to disk.
    
    But instead of complicating the ccb-state machine in ImmModel, just to optimize
    an error case, the solution has been to just drop the send of completed to PBE
    and let this error case merge with the case when the message was successfully
    sent to the PBE yet the PBE restarted before replying.
    
    The problem was that continued execution in this branch implied use of
    the client node, which was missing.
    
    The solution is to goto the end of the function.

--- osaf/services/saf/immsv/immnd/immnd_evt.c
+++ osaf/services/saf/immsv/immnd/immnd_evt.c
@@ -3612,7 +3612,7 @@ static void immnd_evt_proc_ccb_compl_rsp(IMMND_CB *cb,
 			if(pbeConn) {
 				TRACE_5("PBE is LOCAL - send completed upcall for %u", evt->info.ccbUpcallRsp.ccbId);
 				/* The PBE is connected at THIS node. Make the completed upcall. 
-				   If the PBE decides to commit it soes so immediately. 
+				   If the PBE decides to commit it does so immediately. 
 				   It does not wait for the apply upcall.
 				 */
 				SaImmOiHandleT implHandle = m_IMMSV_PACK_HANDLE(pbeConn, pbeNodeId);
@@ -3630,6 +3630,7 @@ static void immnd_evt_proc_ccb_compl_rsp(IMMND_CB *cb,
 					   But actually we know here that we have not sent the completed call to
 					   the pbe. This means it should be easy to abort!
 					 */
+					goto done;
 				}
 
 				memset(&send_evt, '\0', sizeof(IMMSV_EVT));
@@ -3837,6 +3838,7 @@ static void immnd_evt_proc_ccb_compl_rsp(IMMND_CB *cb,
 			/* There is really not much we can do here. */
 		}
 	}
+ done:
 	TRACE_LEAVE();
 }
 
