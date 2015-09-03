eb0779c1f1c4636e5d6e561d0f32f766cb2de7bb
Zoran Milinkovic <zoran.milinkovic@ericsson.com>
2015-09-03 17:13:16 +0200

    imm: fix memory leaks caused by ncs_enc_init_space [#1469]
    In some places memory is allocated by ncs_enc_init_space, but is not freed.
    The fix provides the patch to the memory leaks.

--- osaf/libs/agents/saf/imma/imma_proc.c
+++ osaf/libs/agents/saf/imma/imma_proc.c
@@ -3117,9 +3117,9 @@ SaAisErrorT imma_evt_fake_evs(IMMA_CB *cb,
 	/*Pack the message for sending over multiple hops. */
 
 	if (ncs_enc_init_space(&uba) != NCSCC_RC_SUCCESS) {
-		uba.start = NULL;
 		TRACE_2("ERR_LIBRARY: Failed init ubaid");
-		return SA_AIS_ERR_LIBRARY;
+		rc = SA_AIS_ERR_LIBRARY;
+		goto fail;
 	}
 
 	/* Encode non-flat since we broadcast to unknown receivers. */
@@ -3127,7 +3127,8 @@ SaAisErrorT imma_evt_fake_evs(IMMA_CB *cb,
 
 	if (rc != NCSCC_RC_SUCCESS) {
 		TRACE_2("ERR_LIBRARY: Failed to pre-pack");
-		return SA_AIS_ERR_LIBRARY;
+		rc = SA_AIS_ERR_LIBRARY;
+		goto fail;
 	}
 
 	int32_t size = uba.ttl;
