29e23d457a8103789ccb4abf225ec93bdec7dad8
Anders Widell <anders.widell@ericsson.com>
2016-04-11 13:07:12 +0200

    immtools: Correct code indentation [#1743]
    Correct code indentation to fix the following compilation error discovered when
    using GCC 6:
    
    imm_dumper.cc: In function 'int main(int, char**)':
    imm_dumper.cc:149:5: error: this 'if' clause does not guard... [-Werror=misleading-indentation]
         if ((c = getopt_long(argc, argv, "hp:x:c:a:", long_options, NULL)) == -1)
         ^~
    imm_dumper.cc:152:13: note: ...this statement, but the latter is misleadingly indented as if it is guarded by the 'if'
                 switch (c) {
                 ^~~~~~

--- osaf/tools/safimm/immdump/imm_dumper.cc
+++ osaf/tools/safimm/immdump/imm_dumper.cc
@@ -141,8 +141,8 @@ int main(int argc, char* argv[])
     }
 
     while (1) {
-    if ((c = getopt_long(argc, argv, "hp:x:c:", long_options, NULL)) == -1)
-            break;
+            if ((c = getopt_long(argc, argv, "hp:x:c:", long_options, NULL)) == -1)
+                    break;
 
             switch (c) {
                 case 'h':
